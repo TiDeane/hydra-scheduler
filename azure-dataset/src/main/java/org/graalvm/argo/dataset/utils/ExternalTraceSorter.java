dd31855ae15b835275f4b4b832e4df75049a42e5
Tiago Deane Motta e Silva <tiagodeane@tecnico.ulisboa.pt>
2026-02-18 18:47:05 +0000

    fix: remove star imports

--- azure-dataset/src/main/java/org/graalvm/argo/dataset/utils/ExternalTraceSorter.java
+++ azure-dataset/src/main/java/org/graalvm/argo/dataset/utils/ExternalTraceSorter.java
@@ -1,7 +1,16 @@
 package org.graalvm.argo.dataset.utils;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Collections;
 
 public class ExternalTraceSorter {
 
